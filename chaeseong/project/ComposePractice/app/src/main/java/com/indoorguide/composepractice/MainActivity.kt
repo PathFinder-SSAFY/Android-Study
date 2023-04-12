7a83b57e1554f507e298aabb5defcafef91dadf7
ParkChaeSeong <97psh@naver.com>
2023-04-12 21:50:52 +0900

    [Chae] Practice to use MutableState in Jetpack Compose

---
+++ chaeseong/project/ComposePractice/app/src/main/java/com/indoorguide/composepractice/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.indoorguide.composepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.indoorguide.composepractice.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "$100", style = TextStyle(
+                    color = Color.White,
+                    fontSize = 39.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CreateCircle() {
+    var moneyCounter by remember {
+        mutableStateOf(0)
+    }
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(105.dp)
+            .clickable {
+                       moneyCounter += 1
+            },
+        shape = CircleShape,
+        elevation = 4.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePracticeTheme {
+        MyApp()
+    }
+}
