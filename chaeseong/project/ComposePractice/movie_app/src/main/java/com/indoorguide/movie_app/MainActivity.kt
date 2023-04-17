894b7be85045cf821431ee862c357b39305fc701
ParkChaeSeong <97psh@naver.com>
2023-04-18 01:34:38 +0900

    [Chae] Practice using LazyColumn in Jetpack Compose

--- chaeseong/project/ComposePractice/movie_app/src/main/java/com/indoorguide/movie_app/MainActivity.kt
+++ chaeseong/project/ComposePractice/movie_app/src/main/java/com/indoorguide/movie_app/MainActivity.kt
@@ -3,41 +3,96 @@ package com.indoorguide.movie_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.indoorguide.movie_app.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainContent()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MyApp(content: @Composable () -> Unit) {
+    ComposePracticeTheme {
+        Scaffold(topBar = {
+            TopAppBar(
+                backgroundColor = Color.Magenta,
+                elevation = 5.dp
+            ) {
+                Text(text = "Movies")
+            }
+        }) {
+            Modifier.padding(it)
+            content()
+        }
+    }
+} // End of MyApp
+
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar", "300", "Harry Poter", "Joker"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn() {
+            items(movieList) {
+                MovieRow(it)
+            }
+        }
+    }
+} // End of MainContent
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox,
+                contentDescription = "Movie Image")
+            }
+        }
+        Text(text = movie)
+    }
+} // End of MovieRow
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposePracticeTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
